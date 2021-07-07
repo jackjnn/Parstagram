33fce4ccf821d160e8cc349338421643d13cf3ca
Jack Njoroge <jacknyange@fb.com>
2021-07-06 20:29:08 -0500

    Implemented Logout

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.parstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogout = findViewById(R.id.btnLogin);
+
+        btnLogout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick logout button");
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+            }
+        });
     }
 }
